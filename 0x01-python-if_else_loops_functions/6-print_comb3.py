87bf208ca8e7831be4fbc50f42a4590264af915a
William05Cardozo <3784@holbertonschool.com>
2021-12-21 18:02:02 -0800

    6-Print-Comb

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for a in range(10):
+    for b in range(10):
+        if (a != b and a < b) and a < 9:
+            if a == 8 and b == 9:
+                print("{}{}".format(a, b))
+            else:
+                print("{}{}".format(a, b), end=", ")
