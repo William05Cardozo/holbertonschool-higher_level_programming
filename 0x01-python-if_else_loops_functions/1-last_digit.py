3a46046efc134f602174485e0e134495089f8450
William05Cardozo <3784@holbertonschool.com>
2021-12-21 16:01:53 -0800

    1-Last-Digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,5 @@ if n > 5:
     print("{} {:d} is {:d} and is greater than 5".format(st, number, n))
 if n == 0:
     print("{} {:d} is {:d} is zero".format(st, number, n))
-if n < 6 and number != 0:
+if n < 6 and n != 0:
     print("{} {:d} is {:d} and is less than 6 and not 0".format(st, number, n))
