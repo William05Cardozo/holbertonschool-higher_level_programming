435692fd7f837db22613b32689bfebd8ce02e961
William05Cardozo <3784@holbertonschool.com>
2022-01-18 18:11:52 -0800

    3-Square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class Square:
+    """The class is a Square"""
+
+    def __init__(self, size=0):
+        """Init the class"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+
+"""Define a class"""
+
+
+def area(self):
+    """This class is a area"""
+    """Return the square area"""
+    return(self.__size) ** 2
