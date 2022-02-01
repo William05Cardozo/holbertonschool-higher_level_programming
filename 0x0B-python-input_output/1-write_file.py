a1f0a2d2c68be738736329aba0f30e34d67ed15f
William05Cardozo <3784@holbertonschool.com>
2022-02-01 15:29:34 -0800

    1-Write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@
 def write_file(filename="", text=""):
     """write in the file"""
     with open(filename, 'w', encoding='utf-8') as file:
-        print(file.write(text))
+        return(file.write(text))
