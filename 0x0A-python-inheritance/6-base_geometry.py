fd2ddfefeae16df968cc48c6e50ad3f290bcc7c3
William05Cardozo <3784@holbertonschool.com>
2022-01-31 20:35:41 -0800

    6-BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class BaseGeometry:
+    """the class name is BaseGeometry"""
+    """Define a function"""
+    def area(self):
+        raise Exception("area() is not implemented")
