83bde08fb6f8c7bb9bd4876b83225fa4b1e8f3f5
William05Cardozo <3784@holbertonschool.com>
2022-01-31 21:37:54 -0800

    9-Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Import"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+"""Define a class"""
+
+class Rectangle(BaseGeometry):
+    """The class is a Rectangle"""
+    def __init__(self, width,height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    """Method area"""
+    def area(self):
+        return(self.__width *  self.__height)
+
+    """Method str"""
+    def __str__(self):
+        return("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
