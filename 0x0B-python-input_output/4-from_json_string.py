93888b71dc2cd5eee13da99821687599e6303139
William05Cardozo <3784@holbertonschool.com>
2022-02-01 18:46:17 -0800

    4-Json

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,6 +7,7 @@ import json
 """Define a function"""
 """from_json_string, parametres - my_str"""
 
+
 def from_json_string(my_str):
     """Return an object presented by a Json string"""
     return(json.loads(my_str))
