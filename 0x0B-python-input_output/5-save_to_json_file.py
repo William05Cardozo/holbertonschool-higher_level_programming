4de67af20f398b3e37ef39a6819a980366b88106
William05Cardozo <3784@holbertonschool.com>
2022-02-01 19:16:15 -0800

    5-Json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Import a module"""
+
+
+import json
+
+"""Define a function"""
+"""save_to_json_file, parametres - my_obj, filename"""
+
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as f:
+        return(json.dump(my_obj, f))
