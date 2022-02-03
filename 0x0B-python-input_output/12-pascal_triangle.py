af98912ad6fdcfaa01ee4ee0791069c0f114c31a
William05Cardozo <3784@holbertonschool.com>
2022-02-02 18:03:04 -0800

    12-Pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,5 +9,5 @@ def pascal_triangle(n):
     rows = [[1 for j in range(i + 1)] for i in range(n)]
     for n in range(n):
         for i in range(n - 1):
-            rows[n][i +1] = sum(rows[n - 1][i:i + 2])
+            rows[n][i + 1] = sum(rows[n - 1][i:i + 2])
     return(rows)
