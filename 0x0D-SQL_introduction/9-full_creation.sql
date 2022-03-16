618210154d09074348f52ef7b9f1ba99dca03c1e
William05Cardozo <3784@holbertonschool.com>
2022-03-15 19:21:52 -0700

    9-Full-Creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- Created a new table call second_table
+-- in this table add various archiveds 
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES ('1', 'John', '10');
+INSERT INTO second_table VALUES ('2', 'Alex', '3');
+INSERT INTO second_table VALUES ('3', 'Bob', '14');
+INSERT INTO second_table VALUES ('4', 'George', '8');
