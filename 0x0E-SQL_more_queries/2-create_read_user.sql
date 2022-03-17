117f3073047ea5b68a60f750d35c7c7d8220ff48
William05Cardozo <3784@holbertonschool.com>
2022-03-17 10:25:20 -0700

    2-Create-Read-User

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,5 @@
 -- and new user call user_0d_2 with password
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON htbn_od_2 . * TO 'user_0d_2'@'localhost';
+GRANT SELECT ON htbn_0d_2 . * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGE;
