def76c6d5e5306a6998a8c820ad22164f94d94ed
William05Cardozo <3784@holbertonschool.com>
2022-03-17 01:18:05 -0700

    2-Create-Read-User

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- This script create a new database call hbtn_0d_2
+-- and new user call user_0d_2 with password
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON htbn_od_2 . * TO user_0d_2@localhost;
+FLUSH PRIVILEGESi;
