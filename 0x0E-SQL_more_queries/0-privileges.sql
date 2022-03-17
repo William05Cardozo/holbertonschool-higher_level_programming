baf919bff35589dd9e748f33de129aec49ef7f60
William05Cardozo <3784@holbertonschool.com>
2022-03-17 00:31:00 -0700

    SQL-More-Querie

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- This script show all privilegies in the user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,2 @@
+Project 0x0E - more querie
+Start: 17/03/2.022 - 2:23am
