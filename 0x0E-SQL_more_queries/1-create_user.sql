b902975b34606d8026b50cf867ffa84acbfd9479
William05Cardozo <3784@holbertonschool.com>
2022-03-17 01:05:21 -0700

    1-Create-User

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- This script created an user and add all priviligies
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
