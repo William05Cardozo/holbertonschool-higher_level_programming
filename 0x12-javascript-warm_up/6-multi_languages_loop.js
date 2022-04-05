4b76e1143ffe7ce77ce119e6836f11a62fa1ecd1
William05Cardozo <3784@holbertonschool.com>
2022-04-04 17:31:51 -0700

    Task 6, MultiLanguages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const langu = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const length = langu.length;
+for (let i = 0; i < length; i++) {
+  console.log(langu[i]);
+}
