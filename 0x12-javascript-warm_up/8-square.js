8b15faf2be6a0cbece4163430f80689abdc9bde6
William05Cardozo <3784@holbertonschool.com>
2022-04-04 19:02:31 -0700

    Task 8, Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const num = Number(process.argv[2]);
 let str = 'X';
 if (!num) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 1; i < num; i++) {
     str = str + 'X';
