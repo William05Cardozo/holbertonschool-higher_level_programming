92d698a19f80d137ca20775faed19e1b074b7d3e
William05Cardozo <3784@holbertonschool.com>
2022-05-23 12:12:33 -0700

    Task 1: HBTN_Header_CorrectionCode

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-This script send a request to the url and displays the value of the X-Request-Id
+This script send a request to the url and
+displays the value of the X-Request-Id
 """
 
 from urllib.request import urlopen
 from sys import argv
 
-with urlopen (argv[1]) as response:
+with urlopen(argv[1]) as response:
     res = response.getheader('X-Request-Id')
 print(res)
