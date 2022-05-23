3451b115d59c49153d78dc496e44af2efacd96c5
William05Cardozo <3784@holbertonschool.com>
2022-05-23 15:39:20 -0700

    Task 6: Post_Email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This script send a POST Request to the passed URL,
+with the email
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {'email': argv[2]}
+    r = requests.post(url, data = {'email': value})
+    print(r.text)
