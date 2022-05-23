9136719824366a87ab68d7766a90baa40508c7d2
William05Cardozo <3784@holbertonschool.com>
2022-05-23 11:40:36 -0700

    Task 0: HBTN_Status_CorrectionCode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 This script that fetches https://intranet.hbtn.io/status
 """
 
-import urllib.request
-response = urllib.request.urlopen('https://intranet.hbtn.io/status')
-res = response.read()
+from urllib.request import urlopen
+with urlopen('https://intranet.hbtn.io/status') as response:
+    res = response.read()
 print('Body response:')
 print('\t - type: ' + format(type(res)))
 print('\t - content: ' + str(res))
