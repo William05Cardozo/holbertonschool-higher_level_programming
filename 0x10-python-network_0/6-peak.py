25ee54cf53bebf9869ae3aab2277ea9bc743d8c4
William05Cardozo <3784@holbertonschool.com>
2022-05-23 00:37:27 -0700

    Task 6: Peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Technical Interview:
+Write a function that finds a peak in a list of unsorted integers.
+6-peak.py(this archived) must contain the funtion
+6-peak.txt must contain the complexity of your algorithm
+"""
+
+
+def find_peak(list_of_integers):
+
+    if len(list_of_integers) > 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
