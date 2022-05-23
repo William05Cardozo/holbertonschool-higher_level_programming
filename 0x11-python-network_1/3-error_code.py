64e2939f6409fe8e8dd9318034b0ec79b37265a9
William05Cardozo <3784@holbertonschool.com>
2022-05-23 13:29:22 -0700

    Task 3: Error_Code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script send a request to the URL and
+manage a HTTPError
+"""
+
+import sys from argv
+import urllib.request
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(responde.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print(error.code)
