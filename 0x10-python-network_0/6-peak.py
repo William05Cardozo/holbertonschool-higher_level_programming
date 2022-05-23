db365362eb59695d5de98975cd16560ac7434bd6
William05Cardozo <3784@holbertonschool.com>
2022-05-23 00:40:55 -0700

    Task 6: Peak_Correction_in_Peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,7 @@ Write a function that finds a peak in a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
+    """This function find a peak"""
 
     if len(list_of_integers) > 0:
         list_of_integers.sort()
