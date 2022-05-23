d7f8b63c315c98511f69e064ad66523ed19b227b
William05Cardozo <3784@holbertonschool.com>
2022-05-22 22:40:44 -0700

    Python_Network, Task 0: Body_size, README

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# Send a request and display the size of th body
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,2 @@
+Project - 0x10 - Network#0
+Start: 22/05/2.022 - 12:34am
