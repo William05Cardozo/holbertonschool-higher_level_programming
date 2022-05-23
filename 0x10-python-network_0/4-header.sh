a75b9a651f8baa2993d13c295dcf76a059f514f6
William05Cardozo <3784@holbertonschool.com>
2022-05-22 23:58:52 -0700

    Task 4: Header_CorrectionCode

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script send a GET request to the URL, and displays the body of th respone
-curl "$1" -sX GET -H "X-HolbertonSchool-User-Id:98"
+curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id:98"
