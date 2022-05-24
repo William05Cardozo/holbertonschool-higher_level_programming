68052f2f16f051632c8c27628b7a0ed0e2b9a401
William05Cardozo <3784@holbertonschool.com>
2022-05-23 18:31:32 -0700

    Task 8: JSON_API_CorrectionComment

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This script takes in a letter and send a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+The letter must be a sent in the variable: q
+"""
 
 from sys import argv
 import requests
