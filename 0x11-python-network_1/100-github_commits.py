39c243ea67d0c5751541d1a06487bce270e213a9
William05Cardozo <3784@holbertonschool.com>
2022-05-23 19:00:47 -0700

    Task 10: 100_GITHUB_Commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script show the list of 10 commits
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits"
+    repo = argv[1]
+    owner = argv[2]
+    rq = requests.get(url.format(repo, owner))
+    commit = rq.json()
+    for committ in commit[:10]:
+        print(committ.get('sha'), end=': ')
+        print(committ.get('commit').get('author').get('name'))
