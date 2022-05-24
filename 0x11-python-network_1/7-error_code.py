a802e97c4de26d17ac88a6ee1ae7b689c76168fd
William05Cardozo <3784@holbertonschool.com>
2022-05-23 17:15:14 -0700

    Task 7: Error_code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script send a request to the URL and
+manage a HTTPError
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    try:
+        r.raise_for_status()
+        print(r.text)
+    except Exception as error:
+        print("Error code: {}".format(error.status_code)
